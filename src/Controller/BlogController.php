a1a74dbecf591ba1f753f2e44efbaa1df746a22d
Sergey <sssssergo15@gmail.com>
2020-03-04 16:08:40 +0600

    add Leave comment template

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Entity\Category;
 use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +26,22 @@ class BlogController extends AbstractController
         $posts = $this->postRepository->findAll();
 
         return $this->render('blog/index.html.twig', ['posts' => $posts]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_show")
+     */
+    public function postShow(Post $post)
+    {   
+        return $this->render('/blog/post_show.html.twig', ['post' => $post]);
+    }
+
+    /**
+     * @Route("/comment/{id}/new", name="comment_new")
+     */
+    public function commentNew(Request $request, Post $post)
+    {
+
     }
 
     /**
